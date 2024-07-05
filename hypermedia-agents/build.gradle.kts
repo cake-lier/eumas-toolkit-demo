f2fb8885b06cfd7f4f31341ea25aebf33ac8c1d7
Andrei Ciortea <andrei.ciortea@gmail.com>
2024-07-05 23:03:56 +0200

    refactor: add hypermedia-agents as stanalone project

---
+++ hypermedia-agents/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    java
+    application
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+group = "org.hyperagents.demo"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://raw.github.com/jacamo-lang/mvn-repo/master")
+    }
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases/")  // For current version of Gradle tooling API
+    }
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases-local/") // For older versions of Gradle tooling API
+    }
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+val vertxVersion = "4.5.1"
+val rdf4jVersion = "4.3.9"
+
+dependencies {
+    implementation("org.jacamo:jacamo:1.2.2")
+    implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+    implementation("io.vertx:vertx-web-client")
+    implementation("io.vertx:vertx-web")
+    implementation("com.github.Interactions-HSG:wot-td-java:master-SNAPSHOT")
+    implementation(project(":jacamo-hypermedia:lib"))
+    implementation("org.eclipse.rdf4j:rdf4j-model:$rdf4jVersion")
+    implementation("org.eclipse.rdf4j:rdf4j-rio-api:$rdf4jVersion")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("src/main/java")
+            srcDir("src/main/jade")
+            srcDir("src/main/cartago")
+        }
+        resources {
+            srcDir("src/main/resources")
+            srcDir("src/main/jason")
+            srcDir("src/main/moise")
+        }
+    }
+    test {
+        java {
+            srcDir("test/main/java")
+            srcDir("test/main/jade")
+            srcDir("test/main/cartago")
+        }
+        resources {
+            srcDir("test/main/resources")
+            srcDir("test/main/jason")
+            srcDir("test/main/moise")
+        }
+    }
+}
+
+tasks {
+    register<JavaExec>("runAgents") {
+        description = "Runs the JaCaMo application launching the agents"
+        dependsOn("classes")
+        mainClass = "jacamo.infra.JaCaMoLauncher"
+        args = listOf("hypermedia_agents.jcm")
+        classpath = sourceSets.main.get().runtimeClasspath
+    }
+}
---
+++ hypermedia-agents/src/main/jason/hypermedia_producer.asl
@@ -0,0 +1,27 @@
+!explore.
+
++hypermediaArtifact(ArtifactIRI, ArtifactName, SemanticTypes)[artifact_name(WorkspaceName),artifact_id(WorkspaceId)]
+    :  .member("https://example.org/Source", SemanticTypes) & webid(MyWebID)
+    <- .print("Creating source artifact...");
+       makeArtifact(ArtifactName, "org.hyperagents.jacamo.artifacts.wot.ThingArtifact", [ArtifactIRI], ArtId);
+       setOperatorWebId(MyWebID)[artifact_id(ArtId)];
+       +source_artifact(ArtifactIRI, ArtifactName, ArtId);
+    .
+
++bounded_buffer_artifact(ArtifactIRI, ArtifactName)
+    :  webid(MyWebID) & source_artifact(_, _, SourceArtId)
+    <- makeArtifact(ArtifactName, "org.hyperagents.jacamo.artifacts.wot.ThingArtifact", [ArtifactIRI], BufferArtId);
+       setOperatorWebId(MyWebID)[artifact_id(BufferArtId)];
+       .print("Ready to start production!");
+       !loop(SourceArtId, BufferArtId);
+    .
+
++!loop(SoourceArtId, BufferArtId) : true <-
+    invokeActionWithIntegerOutput("https://example.org/Produce", Item)[artifact_id(SoourceArtId)];
+    .print("Item (", Item, ") has been received");
+    invokeAction("https://example.org/Enqueue", [[Item]])[artifact_id(BufferArtId)];
+    .print("Item (", Item, ") has been enqueued");
+    !loop(SoourceArtId, BufferArtId).
+
+{ include("inc/crawling.asl") }
+{ include("$jacamoJar/templates/common-cartago.asl") }
